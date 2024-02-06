ae460ba4ac2c9213d172d5616107c0ff9621950e
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-02-06 20:48:57 -0300

    configurando consumes na classe ProdutoController

--- src/main/java/io/github/Nog022/SpringBootExpert/apiRest/controller/ProdutoController.java
+++ src/main/java/io/github/Nog022/SpringBootExpert/apiRest/controller/ProdutoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/produtos")
+@RequestMapping(value = "/api/produtos" , consumes = "application/json")
 public class ProdutoController {
 
 	private Produtos produtos;
