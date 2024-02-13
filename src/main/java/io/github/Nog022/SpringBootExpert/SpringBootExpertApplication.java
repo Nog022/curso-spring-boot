957d9936be07fe99d755fa98cd0ba5045c4be51e
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-02-13 12:24:14 -0300

    configurando as informações do spring security

--- src/main/java/io/github/Nog022/SpringBootExpert/SpringBootExpertApplication.java
+++ src/main/java/io/github/Nog022/SpringBootExpert/SpringBootExpertApplication.java
@@ -2,8 +2,10 @@ package io.github.Nog022.SpringBootExpert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class SpringBootExpertApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/github/Nog022/SpringBootExpert/apiRest/controller/SecurityConfig.java
+++ src/main/java/io/github/Nog022/SpringBootExpert/apiRest/controller/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,38 +24,42 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-
-
-//BCryptPasswordEncoder bCryptPasswordEncoder
     @Bean
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("fulano")
-                .password(passwordEncoder().encode("123"))
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
-                .build());
-        manager.createUser(User.withUsername("admin")
-                .password(passwordEncoder().encode("adminPass"))
-                .roles("USER", "ADMIN")
-                .build());
-        return manager;
+                .build();
+
+        UserDetails userAdmin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, userAdmin);
+
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
+        return http.csrf().disable()
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                    .requestMatchers("/api/clientes").permitAll()
+                    .requestMatchers("/api/produtos/**").permitAll()
+                    .requestMatchers("/api/pedidos/**").permitAll()
+                    .anyRequest().authenticated()
+
+                ).httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//        return http.build();
-//    }
 }
+
+
+
+
+
+
